6b22130859a6c57a2591eba2813e1a3d05b198eb
Anisha <gill.anisha@outlook.com>
2022-03-03 18:06:57 -0800

    init commit

--- app.js
+++ app.js
@@ -3,25 +3,15 @@ const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
-
-
-
-
-                // class .todo-input 
-                // enter in bar 
-                // text the user puts in is an input text 
-                // input treated as a constant
-                // find html elements by class name
-
-
 //Event Listeners 
 todoButton.addEventListener("click", addTodo); 
-todoList.addEventListener("click", deleteTodo);
+todoList.addEventListener("click", markTodo);
 
 
 //Functions
 
-function deleteTodo(event) {
+// react to the event target
+function markTodo(event) {
     const item = event.target;
 
     if(item.classList[0] == "trash-btn") {
@@ -32,45 +22,42 @@ function deleteTodo(event) {
             const todo = item.parentElement;
             todo.classList.toggle('completed')
         }
-
-
     }
 }
 
-
-
+// Create a new Todo item with a check mark and trash button
 function addTodo(event) {
     // prevent broswer from refreshing
     event.preventDefault();
 
+    // empty task alert
     if (todoInput.value.length === 0) {
         alert("Please add a task")
         return;
     }
 
-    // create ToDo div
+    // create todo div
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
 
-    // create LI 
+    // create li
     const newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item'); // use todo-item class name to style is css
     todoDiv.appendChild(newTodo); // add li into the div 
 
-    // check mark button 
+    // check button 
     const completedButton = document.createElement("button");
     completedButton.innerHTML = '<i class="fa fa-check"></i>';
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
 
-    // check trash button 
+    // trash button 
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
     trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
 
-    // Append with todo list 
     todoList.append(todoDiv);
 
     // return placeholder
